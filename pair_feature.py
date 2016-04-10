2b332eb409ac9cde5c671c31d851f59fdfc8772e
lapland <xy2283@columbia.edu>
2016-04-09 21:54:30 -0400

    add pair

---
+++ pair_feature.py
@@ -0,0 +1,26 @@
+from distance import edit_dist
+from distance import euclidean_dist
+from distance import jaccard_dist
+from distance import dist_phone
+from distance import my_jaccard_dist
+from distance import dist_address
+
+
+
+
+dist_func_list =[
+    (my_jaccard_dist, 'name')
+    (edit_dist, 'name')
+    (dist_phone, 'phone')
+    (dist_address, 'street_address')
+]
+
+def con_f(a, b):
+    features = []
+    for func, field in dist_func_list:
+        features.append(func(a[field], b[field]))
+    return features
+
+
+ 
+
